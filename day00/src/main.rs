1e5c6c93467ac3c40025c3bb588faa60c0dc95f7
Daste <stefankar1000@gmail.com>
2023-12-01 17:00:55 +0100

    Add template for new days

---
+++ day00/src/main.rs
@@ -0,0 +1,12 @@
+use std::{fs::read_to_string, str::Lines};
+
+fn main() {
+    println!("==== Part 1 output ====");
+    part_1(read_to_string("day00/part1.input.txt").unwrap().lines());
+    println!("\n==== Part 2 output ====");
+    part_2(read_to_string("day00/part2.input.txt").unwrap().lines());
+}
+
+fn part_1(lines: Lines) {}
+
+fn part_2(lines: Lines) {}
