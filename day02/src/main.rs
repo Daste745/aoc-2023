efeaa3d36cddf600a7fa596c66123f7d1ff00e9c
Daste <stefankar1000@gmail.com>
2023-12-02 17:06:01 +0100

    Clean up day 02 solutions

--- day02/src/main.rs
+++ day02/src/main.rs
@@ -11,13 +11,30 @@ const MAX_RED: u32 = 12;
 const MAX_GREEN: u32 = 13;
 const MAX_BLUE: u32 = 14;
 
+fn parse_cube_sets(results_str: &str) -> Vec<Vec<(u32, &str)>> {
+    results_str
+        .split("; ")
+        // "3 blue, 4 red"
+        .map(|set| {
+            set.split(", ")
+                // "3 blue"
+                .map(|color_str| {
+                    let mut color_str_parts = color_str.split(' ');
+                    let color_amount = color_str_parts.next().unwrap().parse::<u32>().unwrap();
+                    let color_name = color_str_parts.next().unwrap();
+                    (color_amount, color_name)
+                })
+                .collect()
+        })
+        .collect()
+}
+
 fn part_1(lines: Lines) {
     let mut total = 0;
 
     for line in lines {
-        let parts: Vec<&str> = line.split(':').collect();
-
-        let game_number = parts
+        let line_parts: Vec<&str> = line.split(": ").collect();
+        let game_number = line_parts
             .first()
             .unwrap()
             .split(' ')
@@ -25,36 +42,23 @@ fn part_1(lines: Lines) {
             .unwrap()
             .parse::<i32>()
             .unwrap();
-
-        // ["3 blue, 4 red", "1 red, 2 green, 6 blue", "2 green"]
-        let cube_sets = parts
-            .last()
-            .unwrap()
-            .split(';')
-            .map(|set| set.strip_prefix(' ').unwrap())
-            .collect::<Vec<&str>>();
+        let cube_sets = parse_cube_sets(line_parts.last().unwrap());
 
         let mut game_possible = true;
         for set in cube_sets {
             // println!("{set}");
-            let colors = set.split(", ").collect::<Vec<&str>>();
-            for color in colors {
-                let color_amount = color.split(' ').next().unwrap().parse::<u32>().unwrap();
-                let color_name = color.split(' ').last().unwrap();
-                let max = match color_name {
+            game_possible = set.iter().all(|(color_amount, color_name)| {
+                let max = match *color_name {
                     "red" => MAX_RED,
                     "green" => MAX_GREEN,
                     "blue" => MAX_BLUE,
                     _ => panic!(),
                 };
-                if color_amount > max {
-                    game_possible = false;
-                    break;
-                }
-            }
+                *color_amount <= max
+            });
             if !game_possible {
                 break;
-            };
+            }
         }
 
         if game_possible {
@@ -72,16 +76,8 @@ fn part_2(lines: Lines) {
     let mut total = 0;
 
     for line in lines {
-        let parts: Vec<&str> = line.split(": ").collect();
-
-        // ["3 blue, 4 red", "1 red, 2 green, 6 blue", "2 green"]
-        let cube_sets = parts
-            .last()
-            .unwrap()
-            .split("; ")
-            // [["3 blue", "4 red"], ["1 red", "2 green", "6 blue"], ["2 green"]]
-            .map(|set| set.split(", ").collect::<Vec<&str>>())
-            .collect::<Vec<Vec<&str>>>();
+        let line_parts: Vec<&str> = line.split(": ").collect();
+        let cube_sets = parse_cube_sets(line_parts.last().unwrap());
 
         let mut max_red: u32 = 0;
         let mut max_green: u32 = 0;
@@ -89,10 +85,7 @@ fn part_2(lines: Lines) {
 
         for set in cube_sets {
             // println!("{set}");
-            for color in set {
-                // "10 red"
-                let color_amount = color.split(' ').next().unwrap().parse::<u32>().unwrap();
-                let color_name = color.split(' ').last().unwrap();
+            for (color_amount, color_name) in set {
                 let max_color = match color_name {
                     "red" => &mut max_red,
                     "green" => &mut max_green,
